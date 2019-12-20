c9b0766234f3bf790a17ebdb1d016655c3a651f6
dochitavasile1987 <dochitavasile1987@gmail.com>
2019-12-20 18:36:09 +0200

    1

---
+++ Week11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week11
+{
+    public class StringUtils
+    {
+        static void Main(string[] args)
+        {
+        }
+        public string Reverse(string input)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = input.Length - 1; i >=0; i--)
+            {
+                sb.Append(input[i]);
+            }
+            return sb.ToString();
+        }
+    }
+
+    
+}
